e2dcf5f22f4da2e04f29b4e75495c179b294659d
InstantFeru <ferpereirared@gmail.com>
2021-11-06 16:46:04 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          1era Entrega
+          1era Entregagit
         </p>
         <a
           className="App-link"
